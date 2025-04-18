8a9dd0affd86be6f6f4427f445fe181f8a1ebeb3
Marcy <87911841+DemonBunnyBon@users.noreply.github.com>
2025-04-18 12:59:39 +0200

    Update version number 2.0.5 -> 2.0.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("76e55258-60b2-4882-8e97-3840eb706dae")]
-[assembly: AssemblyVersion("2.0.5")]
-[assembly: AssemblyFileVersion("2.0.5")]
+[assembly: AssemblyVersion("2.0.6")]
+[assembly: AssemblyFileVersion("2.0.6")]
 [assembly: MelonInfo(typeof(HungerRevamped.HungerRevampedMod), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.ModAuthor)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
@@ -21,5 +21,5 @@ internal static class BuildInfo
 {
     internal const string ModName = "HungerRevamped";
     internal const string ModAuthor = "BA";
-    internal const string ModVersion = "2.0.5";
+    internal const string ModVersion = "2.0.6";
 }
